224ade2925790ac50ebb452f3be040025ab19042
Paulo Lopes <paulo@mlopes.net>
2017-01-12 14:15:34 +0100

    build all

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-set -e
-
-rm -Rf bin || true
+rm -Rf bin
 
 make clean
 make
@@ -11,5 +9,8 @@ make -f Makefile.win32
 
 make -f Makefile.win64 clean
 make -f Makefile.win64
+
+make -f Makefile.mac clean
+make -f Makefile.mac
 
 mvn install
