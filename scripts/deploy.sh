be9f88427c38bf53dbc32e3ab1f3e18a981e0d8f
Paulo Lopes <paulo@mlopes.net>
2017-02-14 13:25:46 +0100

    fix path

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
-if [ -f ./x86_64-linux-gnu/protoc-gen-grpc-java.exe ] && [ -f ./i686-w64-mingw32/protoc-gen-grpc-java.exe ] && [ -f ./x86_64-w64-mingw32/protoc-gen-grpc-java.exe ] && [ -f ./x86_64-apple-darwin14/protoc-gen-grpc-java.exe ]; then
+if [ -f ./target/x86_64-linux-gnu/protoc-gen-grpc-java.exe ] && [ -f ./target/i686-w64-mingw32/protoc-gen-grpc-java.exe ] && [ -f ./target/x86_64-w64-mingw32/protoc-gen-grpc-java.exe ] && [ -f ./target/x86_64-apple-darwin14/protoc-gen-grpc-java.exe ]; then
   PROJECT_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:evaluate -Dexpression=project.version -B | grep -v '\[')
   
   if [[ "${PROJECT_VERSION}" =~ .*SNAPSHOT ]] && [[ "${TRAVIS_BRANCH}" = "master" ]] && [[ "${TRAVIS_PULL_REQUEST}" = "false" ]]; then
